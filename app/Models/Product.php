9f53b57e64fae1532bc4968df47b5fc05664f732
dohaseif2 <dohaseif2@gmail.com>
2024-09-18 23:59:14 +0300

    feat(database): models

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'name', 'quantity', 'image',
+    ];
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
